430ec89c1f9360a29ce43c0a2bf64817b7eede2f
Umoh Udonwa <udonwajnr10@gmail.com>
2024-08-10 23:37:45 -0700

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+const connectDb = require("./config/db")
+const color = require("colors")
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+const port =3000
+app.listen(port,()=>{
+    console.log("I'm back")
+})
+
+connectDb()
