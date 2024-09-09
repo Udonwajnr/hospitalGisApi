867fef6db2fe99ffedf5bf9809fbc48211ff597d
Umoh Udonwa <udonwajnr10@gmail.com>
2024-09-09 15:38:28 -0700

    commit

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const port=3000
 
 app.use(cookieParser())
 app.use(cors())
+const corsOptions = {
+    origin: 'https://hospital-admin-neon.vercel.app', // Replace with your client application's URL
+    methods: 'GET,POST,PUT,DELETE', // Allow the HTTP methods you need
+    allowedHeaders: 'Content-Type,Authorization', // Allow the headers you need
+  };
+  
+  // Use CORS middleware with options
+  app.use(cors(corsOptions));
+  
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
